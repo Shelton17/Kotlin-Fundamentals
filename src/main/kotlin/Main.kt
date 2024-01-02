57afe82f55cc90ff0da1dd6b93c28d9604a1ca29
Shelton17 <okangajeff17@gmail.com>
2024-01-02 19:01:44 +0300

    combining age and favorite number

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,25 @@
 fun main() {
-//  outputing varios strings
+//      outputing varios strings
     println("Hello World!")
     println("I'm Awesome")
-//  variables in kotlin
+//      variables in kotlin
     var myVariable: Int = 5
     println(myVariable)
     println("The value of our vaiable is: $myVariable")
     myVariable = 1
     println("The value is now: $myVariable")
 
-//  boolean variables
+//      boolean variables
     var booVariable: Boolean = true
     println("The value of our vaiable is: $booVariable")
     booVariable = false
     println("The value is now: $booVariable")
 
-//  assignment: saving my age and favorite number
+//      assignment: saving my age and favorite number
     var ageVariable: Int = 23
     println("I am $ageVariable years old")
     var favNumber: Int = 17
     println("My favorite number is $favNumber")
+    println("I am $ageVariable years old and my favoritee number is $favNumber")
 
 }

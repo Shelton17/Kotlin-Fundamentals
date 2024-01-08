ee8270a685a135edeeb9d6b3a0fc6878c33e41ea
Shelton17 <okangajeff17@gmail.com>
2024-01-08 20:47:21 +0300

    Add: Learning about classes

---
+++ src/main/kotlin/ClassesInKt.kt
@@ -0,0 +1,6 @@
+fun main(){
+    val myRect = Rectangle(4.0,7.0)
+    println("Rectangle area is ${myRect.area()}")
+    println("Rectangle perimeter is ${myRect.perimeter()}")
+    println("Is rectangle a square? ${myRect.isSquare()}")
+}
